2b7b4a74223aa130c6b5bf44a147896678e78fd9
PlanJames <joselencina0612@gmail.com>
2024-11-07 18:58:19 -0300

    Agrego cosas al panel register

--- forms/register/form_register_designer.py
+++ forms/register/form_register_designer.py
@@ -1,13 +1,13 @@
 import tkinter as tk
 from tkinter import messagebox
 import sqlite3
+from tkinter.font import BOLD
 import util.generic as utl
 from tkinter import ttk
 from data.base_de_datos import BaseDeDatos  
 
-
 class RegisterUsuario:
-    
+
     def __init__(self):
         self.ventana = tk.Tk()                             
         self.ventana.title('Registro')
@@ -29,5 +29,22 @@ class RegisterUsuario:
         #frame form fill
         frame_form_fill = tk.Frame(frame_form,height = 50,  bd=0, relief=tk.SOLID,bg='#fcfcfc')
         frame_form_fill.pack(side="bottom",expand=tk.YES,fill=tk.BOTH)
+
+        etiqueta_newuser = tk.Label(frame_form_fill, text="Ingresa un usuario", font=('Times', 14) ,fg="#666a88",bg='#fcfcfc', anchor="w")
+        etiqueta_newuser.pack(fill=tk.X, padx=20,pady=5)
+        self.newusuario = ttk.Entry(frame_form_fill, font=('Times', 14))
+        self.newusuario.pack(fill=tk.X, padx=20,pady=10)
+
+        etiqueta_password = tk.Label(frame_form_fill, text="Crea una contraseña", font=('Times', 14),fg="#666a88",bg='#fcfcfc' , anchor="w")
+        etiqueta_password.pack(fill=tk.X, padx=20,pady=5)
+        self.password = ttk.Entry(frame_form_fill, font=('Times', 14))
+        self.password.pack(fill=tk.X, padx=20,pady=10)
+
+        etiqueta_secondpassword = tk.Label(frame_form_fill, text="Repite la contraseña", font=('Times', 14),fg="#666a88",bg='#fcfcfc' , anchor="w")
+        etiqueta_secondpassword.pack(fill=tk.X, padx=20,pady=5)
+        self.password = ttk.Entry(frame_form_fill, font=('Times', 14))
+        self.password.pack(fill=tk.X, padx=20,pady=10)
+        self.password.config(show="*")
+
 
         self.ventana.mainloop()
